a5f4b731e8464102522bcfa75e67ccd5c4850553
atarannn <anastasiatarannn@icloud.com>
2022-10-23 00:20:22 +0300

    main commit

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,11 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Accounting of employees in the company</h1>
+        </div>
+    )
+}
+
+export default AppInfo;
